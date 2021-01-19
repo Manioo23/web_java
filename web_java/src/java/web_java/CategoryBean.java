85cf77cad2fdeaee11fcdf72d0b41a19e6f611bf
bkrzeptowski <bkrzeptowski@wp.pl>
2021-01-19 01:22:39 +0100

    Sklep

---
+++ web_java/src/java/web_java/CategoryBean.java
@@ -0,0 +1,126 @@
+package web_java;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author JD
+ */
+@ManagedBean( name="categoryBean" )
+    public class CategoryBean implements Serializable
+       {
+          private String category;
+          private String choice;
+          public HashMap<String, String> products = new HashMap<String, String>();
+          public HashMap<String, String> prices = new HashMap<String, String>();
+          
+          CategoryBean(){
+            products.put("WHEY DELICIOUS PROTEIN - 700g", "odzywki_bialkowe");
+            products.put("WHEY PROTEIN COMPLEX 100% - 700g", "odzywki_bialkowe");
+            products.put("ULTRA WHEY - 908g", "odzywki_bialkowe");
+            products.put("CREATINE XPLODE - 500g", "kreatyny");
+            products.put("CREA X3 - 400g", "kreatyny");
+            products.put("CELL TECH PERFORMANCE SERIES - 2270g", "kreatyny" );
+            products.put("PROFI MASS - 1000g", "odzywki_na_mase");
+            products.put("HARD MASS - 1000g", "odzywki_na_mase");
+            products.put("DIET FUEL ULTRALEAN - 2000g", "odzywki_na_mase");
+            prices.put("WHEY DELICIOUS PROTEIN - 700g", "120zl");
+            prices.put("WHEY PROTEIN COMPLEX 100% - 700g", "99zl");
+            prices.put("ULTRA WHEY - 908g", "115zl");
+            prices.put("CREATINE XPLODE - 500g", "39zl");
+            prices.put("CREA X3 - 400g", "59zl");
+            prices.put("CELL TECH PERFORMANCE SERIES - 2270g", "239zl");
+            prices.put("PROFI MASS - 1000g", "89zl");
+            prices.put("HARD MASS - 1000g", "119zl");
+            prices.put("DIET FUEL ULTRALEAN - 2000g", "249zl");
+            
+          }
+          
+          // return the name String
+          public String getCategory()
+          {
+             return category;
+          } // end method getName
+          
+          // set the name String
+          public void setCategory( String _category )
+          {
+             this.category = _category;
+          } // end method setName
+          
+          public String getChoices()
+          {
+             return choices;
+          } // end method getName
+          
+          // set the name String
+          public void setChoices( String[] _choices )
+          {
+             this.choices = choices;
+          } // end method setName
+
+          public String getBialka()
+          {
+            String[] lista;
+            int i = 0;
+            lista = new String[3];
+            for (Map.Entry<String, String> entry : products.entrySet()) {
+                if (entry.getValue() == "odzywki_bialkowe"){
+                    lista[i] = entry.getKey();
+                    i++;
+                    }
+                }
+                return lista;
+            }
+
+            public String getKreatyna()
+            {
+            String[] lista;
+            int i = 0;
+            lista = new String[3];
+            for (Map.Entry<String, String> entry : products.entrySet()) {
+                if (entry.getValue() == "kreatyna"){
+                    lista[i] = entry.getKey();
+                    i++;
+                }
+            }
+            return lista;
+            }
+
+            public String getMasa()
+          {
+            String[] lista;
+            int i = 0;
+            lista = new String[3];
+            for (Map.Entry<String, String> entry : products.entrySet()) {
+                if (entry.getValue() == "odzywki_na_mase"){
+                    lista[i] = entry.getKey();
+                    i++;
+                }
+            }
+            return lista;
+            }
+
+            public String getData(){
+                if (category == "odzywki_bialkowe"){
+                    return getBialka();
+                }
+                if else (category == "kreatyna"){
+                    return getKreatyna();
+                }
+                else {
+                    return getMasa();
+                }
+            }
+
+            public String getCena(nazwa){
+                return prices.get(nazwa);
+            }
+            
+
+            
+          
+       } // end class ValidationBean
